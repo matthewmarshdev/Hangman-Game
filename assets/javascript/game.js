8e006bfd67a747d652e3e9dc788dc907c53b338d
Matthew <matthewmarshdev@gmail.com^C>
2017-12-04 17:30:07 -0700

    final coffee add

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
 var words = ['rickroll', 'nyancat', 'orly', 'feelsbadman', 'joebiden'];
+var wordGetter = math.floor(math.random()*10);
 
 function wordChoice () {
 
-	
+ 
+    for(var i = 0; i < words.length(); i++){
+        
+    }
+
+document.getElementById("demo").innerHTML = fruits.toString();
 }
